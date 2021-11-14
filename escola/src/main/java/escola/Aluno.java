d7061fd85fc53d7bf8f57bc382bae74f27b8e358
alessandrasouzas <le16bits@gmail.com>
2021-11-13 22:47:43 -0300

    cria a classe fabrica de aluno

--- escola/src/main/java/escola/Aluno.java
+++ escola/src/main/java/escola/Aluno.java
@@ -3,22 +3,41 @@ package escola;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Aluno {
 	
-	private CPF cpf;	
-	private String nome;
-	
+	private CPF cpf;
+	private String nome;	
 	private Email email;
 	
 	private List<Telefone> telefones = new ArrayList<>();
 	
+	public Aluno(CPF cpf, String nome, Email email) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.email = email;
+	}
+
 	public void adicionarTelefone(String ddd, String numero) {
 		this.telefones.add(new Telefone(ddd, numero));
 	}
+	
+	public CPF getCpf() {
+		return cpf;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Email getEmail() {
+		return email;
+	}
 
+	
 }
+
+
+
+
+
+
